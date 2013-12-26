6932aff2d8bbc292cd11c35d1e65305630882a8c
Henare Degan <henare.degan@gmail.com>
2013-12-25 21:51:38 -0300

    Don't bail out when some attributes don't exist for an issue

--- lib/jira_item.rb
+++ lib/jira_item.rb
@@ -4,15 +4,21 @@ class JiraItem
     {title: item.at(:title).inner_text,
      description: item.at(:description).inner_text,
      type: item.at(:type).inner_text,
-     priority: item.at(:priority).inner_text,
+     priority: inner_text_or_nil(item.at(:priority)),
      status: item.at(:status).inner_text,
      resolution: item.at(:resolution).inner_text,
      reporter: item.at(:reporter).inner_text,
      assignee: item.at(:assignee).inner_text,
      created: item.at(:created).inner_text,
      updated: item.at(:updated).inner_text,
-     fixVersion: item.at(:fixVersion).inner_text,
-     component: item.at(:component).inner_text}
+     fixVersion: inner_text_or_nil(item.at :fixVersion),
+     component: inner_text_or_nil(item.at :component)}
      # attachment: item.at(:attachment).inner_text
+  end
+
+  private
+
+  def self.inner_text_or_nil(element)
+    element.inner_text if element
   end
 end
