9d8b6695624fcb852bbd6d4b915d54c642bb2e4e
Henare Degan <henare.degan@gmail.com>
2013-12-26 02:52:21 -0300

    Start work on formatting the issue body

--- issue_monkeypatch.rb
+++ issue_monkeypatch.rb
@@ -6,8 +6,30 @@ module JIRA
       end
 
       def body
+        assignee = assignee ? assignee.displayName : 'Unassigned'
+
         # TODO: Add more information from the issue to this for GitHub
-        self.description
+        <<-BODY.gsub(/^ {10}/, '')
+          #{self.description}
+
+          ---
+          **Issue details imported from Jira:**
+
+          Detail | Value
+          ------ | -----
+          Assignee | #{assignee}
+          Reporter | #{self.reporter.displayName}
+          Created | #{Date.parse(self.created).to_s}
+          Updated | #{Date.parse(self.created).to_s}
+
+          ## Comments
+          TODO
+        BODY
+      end
+
+      def github_labels
+        # TODO: Output labels, e.g.
+        # ['Task', 'High priority', 'Component: Application', 'Version: Some version']
       end
     end
   end
--- migrate.rb
+++ migrate.rb
@@ -22,15 +22,8 @@ puts "Geting Jira issues for project key #{configuration['jira']['project_key']}
 jira_issues = client.Project.find(configuration['jira']['project_key']).issues
 puts "Found #{jira_issues.count} Jira issues"
 
-def jira_to_github(jira_issue)
-  # TODO: Add GitHub issue labels for additional Jira attributes e.g. issue type, priority, etc.
-  {title: jira_issue.title,
-   body: jira_issue.body}
+jira_issues.each do |issue|
+  puts "Creating GitHub issue #{issue.title}..."
+  github_issue = Octokit.create_issue configuration['github']['repo'], issue.title, issue.body, {labels: issue.labels}
+  puts "Created GitHub issue ##{github_issue.number}"
 end
-
-# jira_issues.each do |jira_issue|
-#   issue = jira_to_github(jira_issue)
-#   puts "Creating GitHub issue #{issue[:title]}..."
-#   github_issue = Octokit.create_issue configuration['github']['repo'], issue[:title], issue[:body]
-#   puts "Created GitHub issue ##{github_issue.number}"
-# end
